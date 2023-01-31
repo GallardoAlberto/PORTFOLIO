12b6675929d26650151a2c815c22489de87bdf58
GallardoAlberto <gallardo.alb.1998@gmail.com>
2023-01-31 10:25:43 +0100

    actualizado, cosas como los tipos de datos que se pueden meter, quedan por pulir

--- js/functions.js
+++ js/functions.js
@@ -1,81 +1,98 @@
 // apartado de skills
 
-let skills = document.querySelector('#skills');
-let skillsh2 = skills.querySelectorAll('h2');
-let skillscertificated = skills.querySelectorAll('.container--certificate');
+let skills = document.querySelector("#skills");
+let skillsh2 = skills.querySelectorAll("h2");
+let skillscertificated = skills.querySelectorAll(".container--certificate");
 
-let skillhtml = skills.querySelector('.fa-html5');
-let skillcss = skills.querySelector('.fa-css3-alt');
-let skilljs = skills.querySelector('.fa-js');
-let skillphp = skills.querySelector('.fa-php');
-let skillglobal = skills.querySelector('.fa-globe');
+let skillhtml = skills.querySelector(".fa-html5");
+let skillcss = skills.querySelector(".fa-css3-alt");
+let skilljs = skills.querySelector(".fa-js");
+let skillphp = skills.querySelector(".fa-php");
+let skillglobal = skills.querySelector(".fa-globe");
 
-skillhtml.addEventListener('click', () =>{
-    skillsh2[0].classList.toggle('hide');
-    skillscertificated[0].classList.toggle('hide');
+skillhtml.addEventListener("click", () => {
+  skillsh2[0].classList.toggle("hide");
+  skillscertificated[0].classList.toggle("hide");
 });
 
-skillcss.addEventListener('click', () =>{
-    skillsh2[1].classList.toggle('hide');
-    skillscertificated[1].classList.toggle('hide');
+skillcss.addEventListener("click", () => {
+  skillsh2[1].classList.toggle("hide");
+  skillscertificated[1].classList.toggle("hide");
 });
 
-skilljs.addEventListener('click', () =>{
-    skillsh2[2].classList.toggle('hide');
-    skillscertificated[2].classList.toggle('hide');
+skilljs.addEventListener("click", () => {
+  skillsh2[2].classList.toggle("hide");
+  skillscertificated[2].classList.toggle("hide");
 });
 
-skillglobal.addEventListener('click', () =>{
-    skillsh2[3].classList.toggle('hide');
-    skillscertificated[3].classList.toggle('hide');
+skillglobal.addEventListener("click", () => {
+  skillsh2[3].classList.toggle("hide");
+  skillscertificated[3].classList.toggle("hide");
 });
 
-skillphp.addEventListener('click', () =>{
-    skillsh2[4].classList.toggle('hide');
-    skillscertificated[4].classList.toggle('hide');
+skillphp.addEventListener("click", () => {
+  skillsh2[4].classList.toggle("hide");
+  skillscertificated[4].classList.toggle("hide");
 });
 
 // boton de subir al inicio
 
-let buttonhome = document.querySelector('.fa-chevron-up');
+let buttonhome = document.querySelector(".fa-chevron-up");
 
-buttonhome.addEventListener('click', () => {
-    window.location.href = "#home";
-})
+buttonhome.addEventListener("click", () => {
+  window.location.href = "#home";
+});
 
 // menu movil
 
-let mobilemenu = document.querySelector('.container--home--menuMobile')
-let buttonmenu = document.querySelector('.fa-bars');
-let contentmenu = mobilemenu.querySelector('ul');
+let mobilemenu = document.querySelector(".container--home--menuMobile");
+let buttonmenu = document.querySelector(".fa-bars");
+let contentmenu = mobilemenu.querySelector("ul");
 
 menuClick = false;
 
-buttonmenu.addEventListener('click', ()=>{
+buttonmenu.addEventListener("click", () => {
+  if (menuClick == false) {
+    setTimeout(() => {
+      buttonhome.style.display = "none";
+      contentmenu.style.opacity = "1";
+      mobilemenu.style.backgroundColor = "black";
+      mobilemenu.style.opacity = "0.9";
+      mobilemenu.style.height = "100vh";
+    }, 150);
+    contentmenu.style.display = "block";
 
-    if(menuClick == false){
-        setTimeout(()=>{
-            buttonhome.style.display = 'none';
-            contentmenu.style.opacity = '1';
-            mobilemenu.style.backgroundColor = 'black';
-            mobilemenu.style.opacity = '0.9';
-            mobilemenu.style.height = '100vh';
+    menuClick = true;
+  } else {
+    setTimeout(() => {
+      contentmenu.style.opacity = "0";
+      mobilemenu.style.backgroundColor = "transparent";
+      buttonhome.style.display = "block";
+      mobilemenu.style.height = "0";
+    }, 150);
+    contentmenu.style.display = "none";
 
-        },150);
-        contentmenu.style.display = 'block';
+    menuClick = false;
+  }
+});
 
-        menuClick = true;
-    }else{
-        setTimeout(()=>{
-            contentmenu.style.opacity = '0';
-            mobilemenu.style.backgroundColor = 'transparent';
-            buttonhome.style.display = 'block';
-            mobilemenu.style.height = '0';
+// esconder y mostrar menu
 
-        },150);
-        contentmenu.style.display = 'none';
+let menu = document.querySelector(".container--home");
+let spacewhite = document.querySelector('#space');
+let prevScrollPos = window.pageYOffset;
 
-        menuClick = false;
+window.onscroll = ()=> {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollPos >= currentScrollPos) {
+    menu.style.top = "0";
+  } else {
+    menu.style.top = "-102px";
+    contentmenu.style.display = "none";
+    mobilemenu.style.backgroundColor = "black";
+    mobilemenu.style.height = "0";
+    menuClick = false;
 
-    }
-})
+  }
+  prevScrollPos = currentScrollPos;
+};
--- php/conexionBBDD.php
+++ php/conexionBBDD.php
@@ -22,6 +22,49 @@
 
         mysqli_close($connect);
 
+        $myemail = "gallardo.alb.1998@gmail.com";
+        $subject = "Portfolio - Formulario";
+        $message = "
+        <html>
+            <head>
+            <title>Mensaje del Portfolio</title>
+            <style>
+                table {
+                    border-collapse: collapse;
+                }
+                th, td {
+                    border: 1px solid black;
+                    padding: 8px;
+                }
+            </style>
+            </head>
+            <body>
+                <table>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Teléfono</th>
+                        <th>E-mail</th>
+                        <th>Mensaje</th>
+                    </tr>
+                    <tr>
+                        <td>$name</td>
+                        <td>$phone</td>
+                        <td>$email</td>
+                        <td>$message</td>
+                    </tr>
+                </table>
+            </body>
+        </html>
+        ";
+
+        // Cabecera para indicar que es un correo HTML
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "From: $email" . "\r\n";
+
+        // Enviar el correo
+        mail($myemail,$subject,$message,$headers);
+
         sleep(2);
         header("Location: ../index.html");
     }
